645191502dd2c5eb84b50b0e82e68192f76e168e
MasliaievKirill <kirillmaslyaev@gmail.com>
2021-11-27 09:31:36 +0200

    Add NetworkUtils and JSONUtils

--- app/src/main/java/com/masliaiev/filmspace/MainActivity.java
+++ app/src/main/java/com/masliaiev/filmspace/MainActivity.java
@@ -3,6 +3,16 @@ package com.masliaiev.filmspace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.masliaiev.filmspace.data.Movie;
+import com.masliaiev.filmspace.utils.JSONUtils;
+import com.masliaiev.filmspace.utils.NetworkUtils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 3);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for (Movie movie : movies) {
+            builder.append(movie.getTitle()).append("\n");
+        }
+        Log.i("MyResult", builder.toString());
     }
 }
